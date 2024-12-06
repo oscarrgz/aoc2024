09db58749d2a643fcb27fde1e149f246f70019e0
oscar <oscar.rgz.fdz@gmail.com>
2024-12-06 12:25:35 +0100

    [day04] start of puzzle 1 day 4

---
+++ day04/day04.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+from typing import List
+
+
+def read_input(file_path: Path) -> List[str]:
+    """Read input file
+
+    :param Path file_path: Path to the input file
+    :return str: Read string
+    """
+
+    data_lines = []
+    with open(file_path, "r") as in_file:
+        data_lines = in_file.readlines()
+
+    return data_lines
+
+
+def find_count_target_word(search_map: List[str], target_word: str = "XMAS") -> int:
+    """Find count of a target word
+
+    Find the total appearances of the target word in the search map
+    array of strings. The target word migh appear horizontally, vertically,
+    diaganoally and might be backwargs
+
+    :param List[str] search_map: search space, assumed that length of strings is constant
+    :param str target_word: Word to be searched for, defaults to "XSMAS"
+    :return int: Number of word appearances
+    """
+
+    word_length = len(target_word)
+    n_cols = len(search_map[0])
+    n_rows = len(search_map)
+    count = 0
+
+    for i_row in range(0, n_rows):
+        for j_col in range(0, n_cols):
+            if search_map[i_row][j_col] != target_word[0]:
+                continue
+
+            # Look horizontally forward:
+            if j_col + word_length <= n_cols:
+                count += (
+                    1
+                    if search_map[i_row][j_col : j_col + word_length] == target_word
+                    else 0
+                )
+            # Look horizontally backwards
+            if j_col - word_length + 1 > 0:
+                count += (
+                    1
+                    if search_map[i_row][j_col : j_col - word_length : -1]
+                    == target_word
+                    else 0
+                )
+            elif j_col + 1 == word_length:
+                count += 1 if search_map[i_row][j_col::-1] == target_word else 0
+
+            # Look vertically downwards:
+            if j_col + word_length <= n_cols:
+                count += (
+                    1
+                    if search_map[j_col : j_col + word_length][j_col] == target_word
+                    else 0
+                )
+            # Look horizontally backwards
+            if j_col - word_length + 1 > 0:
+                count += (
+                    1
+                    if search_map[i_row][j_col : j_col - word_length : -1]
+                    == target_word
+                    else 0
+                )
+            elif j_col + 1 == word_length:
+                count += 1 if search_map[i_row][j_col::-1] == target_word else 0
