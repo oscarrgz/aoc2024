b974cd71844894a95a61586f5cc9d9b61dbaa6a4
oscar <oscar@example.com>
2024-12-06 19:20:21 +0100

    [day04] solution to part 2

--- day04/day04.py
+++ day04/day04.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from typing import List
 import numpy as np
+from numpy.typing import NDArray
 
 
-def read_input(file_path: Path) -> List[str]:
+def read_input(file_path: Path) -> NDArray:
     """Read input file
 
     :param Path file_path: Path to the input file
@@ -18,7 +19,7 @@ def read_input(file_path: Path) -> List[str]:
     return np.asarray(data_lines)
 
 
-def find_count_target_word(search_map: List[str], target_word: str = "XMAS") -> int:
+def find_count_target_word(search_map: NDArray, target_word: str = "XMAS") -> int:
     """Find count of a target word
 
     Find the total appearances of the target word in the search map
@@ -101,10 +102,53 @@ def find_count_target_word(search_map: List[str], target_word: str = "XMAS") -> 
     return count
 
 
+def find_count_mas_in_cross(search_map: NDArray) -> int:
+    """Find counts of MAS forming a cross
+
+    Count the times that MAS appears forming a cross lik
+    M . S
+    - A .
+    M . S
+
+    :param NDArray search_map: List of words to search for
+    :return int: Number of appearances
+    """
+
+    count = 0
+    target_word = "MAS"
+    targets = [target_word, "".join(reversed(target_word))]
+    n_cols = len(search_map[0])
+    n_rows = len(search_map)
+    word_length = len(target_word)
+
+    for i_row in range(0, n_rows):
+        for j_col in range(0, n_cols):
+            if search_map[i_row, j_col] != target_word[1]:
+                continue
+
+            if (
+                j_col != 0
+                and i_row != 0
+                and j_col != n_cols - 1
+                and i_row != n_rows - 1
+            ):
+                word_1 = "".join(
+                    [search_map[i_row + x, j_col + x] for x in range(-1, 2)]
+                )
+                word_2 = "".join(
+                    [search_map[i_row + x, j_col - x] for x in range(-1, 2)]
+                )
+                if word_1 in targets and word_2 in targets:
+                    count += 1
+    return count
+
+
 def solve_puzzle():
     input_instructions = read_input(Path(__file__).parent / "input.txt")
     solution_part_1 = find_count_target_word(input_instructions)
     print(f"Solution to puzzle 1 is {solution_part_1}")
+    solution_part_2 = find_count_mas_in_cross(input_instructions)
+    print(f"Solution to puzzle 2 is {solution_part_2}")
 
 
 if __name__ == "__main__":
